2c4e83eb2d5bc828cacc5c54667964d92282fdeb
Jan Kotas <jkotas@microsoft.com>
2023-06-18 19:16:38 -0700

    Add dotnet benchmarks

---
+++ dotnet-async/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Diagnostics;
+
+for (int i = 0; i < 10; i++)
+{
+    var sw = new Stopwatch();
+    sw.Start();
+    int result = await A(100000);
+    Console.WriteLine($"{sw.ElapsedMilliseconds} ms result={result}");
+}
+
+static async ValueTask<int> A(int n)
+{
+    int result = 1;
+    for (int i = 1; i < n; i++)
+        result = (result + i) * await B(i);
+    return result;
+}
+
+static async ValueTask<int> B(int n)
+{
+    int result = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (result == 1_000_000)
+            await Task.Yield();
+        result += i * i;
+    }
+    return result;
+}
---
+++ dotnet-sync/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Diagnostics;
+using System.Threading;
+
+for (int i = 0; i < 10; i++)
+{
+    var sw = new Stopwatch();
+    sw.Start();
+    int result = A(100000);
+    Console.WriteLine($"{sw.ElapsedMilliseconds} ms result={result}");
+}
+
+static int A(int n)
+{
+    int result = 1;
+    for (int i = 1; i < n; i++)
+        result = (result + i) * B(i);
+    return result;
+}
+
+static int B(int n)
+{
+    int result = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (result == 1_000_000)
+            Thread.Yield();
+        result += i * i;
+    }
+    return result;
+}
